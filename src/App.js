e88d54103cfb64d5b04ccd6111a7039b0637d745
Somesh Chandimal <someshchandih@gmail.com>
2023-08-22 17:45:54 +0530

    login page created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import {
   Login,
@@ -15,44 +15,58 @@ import {
   UserRoles,
 } from "./screens";
 import NavBar from "./components/NavBar/navBar";
+import UserContext from "./context/UserContext";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <NavBar />
-        <div className="row" style={{ backgroundColor: "#282C35" }}>
-          {/* <div className="col-3"><SideBar /></div> */}
-          <div
-            className="col"
-            style={{ backgroundColor: "white", borderWidth: 5 }}
-          >
-            <Switch>
-              {/* Employees Management */}
-              <Route path="/employee/new" component={EmployeeForm} />
-              <Route path="/employee/roles" component={UserRoles} />
-              <Route path="/employee" component={EmployeeList} />
-              {/* Inventory Management */}
-              <Route path="/inventory/new" component={ProductForm} />
-              <Route path="/inventory/categories" component={Categories} />
-              <Route path="/inventory/discounts" component={Discount} />
-              <Route path="/inventory" component={Inventory} />
-              {/* Customer Management */}
-              <Route path="/customers/new" component={CustomerForm} />
-              <Route path="/customers" component={Customers} />
-              {/* Common */}
-              <Route path="/dashboard" component={Dashboard} />
-              <Route path="/login" component={Login} />
-              <Route path="/not-found" component={NotFound} />
-              <Redirect from="/" exact to="/dashboard" />
-              <Redirect to="/not-found" />
-            </Switch>
+function App() {
+  // const [currentUser, setCurrentUser] = useState({ name: "somesh Chandimal" });
+  const [currentUser, setCurrentUser] = useState(null);
+
+  return (
+    <UserContext.Provider
+      value={{ currentUser: currentUser, setCurrentUser: setCurrentUser }}
+    >
+      {!currentUser && (
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Redirect to="/login" />
+        </Switch>
+      )}
+      {currentUser && (
+        <React.Fragment>
+          <NavBar />
+          <div className="row" style={{ backgroundColor: "#282C35" }}>
+            {/* <div className="col-3"><SideBar /></div> */}
+            <div
+              className="col"
+              style={{ backgroundColor: "white", borderWidth: 5 }}
+            >
+              <Switch>
+                {/* Employees Management */}
+                <Route path="/employee/new" component={EmployeeForm} />
+                <Route path="/employee/roles" component={UserRoles} />
+                <Route path="/employee" component={EmployeeList} />
+                {/* Inventory Management */}
+                <Route path="/inventory/new" component={ProductForm} />
+                <Route path="/inventory/categories" component={Categories} />
+                <Route path="/inventory/discounts" component={Discount} />
+                <Route path="/inventory" component={Inventory} />
+                {/* Customer Management */}
+                <Route path="/customers/new" component={CustomerForm} />
+                <Route path="/customers" component={Customers} />
+                {/* Common */}
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/login" component={Login} />
+                <Route path="/not-found" component={NotFound} />
+                <Redirect from="/" exact to="/dashboard" />
+                <Redirect to="/not-found" />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </React.Fragment>
-    );
-  }
+        </React.Fragment>
+      )}
+    </UserContext.Provider>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min";
 
@@ -11,9 +11,11 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
